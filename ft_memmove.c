e50134e55686eb3d31a9e6b61429a67fc66575db
mstaali <imthetigerz@gmail.com>
2023-11-16 20:10:19 +0100

    buffer_elim

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mstaali <mstaali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 17:10:26 by mstaali           #+#    #+#             */
-/*   Updated: 2023/11/02 21:37:21 by mstaali          ###   ########.fr       */
+/*   Updated: 2023/11/16 20:07:06 by mstaali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	*ft_memmove(void *dest, const void *src, size_t size)
 {
 	char		*d;
 	const char	*s;
-	char		buffer[1024];
 
 	d = (char *)dest;
 	s = (const char *)src;
@@ -24,8 +23,8 @@ void	*ft_memmove(void *dest, const void *src, size_t size)
 		return (dest);
 	if (d > s && d < (char *)s + size)
 	{
-		ft_memcpy(buffer, src, size);
-		ft_memcpy(dest, buffer, size);
+		while (size--)
+			*(char *)(dest + size) = *(char *)(src + size);
 	}
 	else
 		ft_memcpy(dest, src, size);
