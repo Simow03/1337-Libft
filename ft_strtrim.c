6fca62bb855d1fe905ad3aaff2ba98dcb51e2c9e
mstaali <imthetigerz@gmail.com>
2023-11-29 18:47:04 +0100

    protect_segf

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mstaali <mstaali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:56:23 by mstaali           #+#    #+#             */
-/*   Updated: 2023/11/06 20:42:05 by mstaali          ###   ########.fr       */
+/*   Updated: 2023/11/25 23:58:39 by mstaali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ char	*ft_strtrim(const char *s1, const char *set)
 	size_t	start;
 	size_t	end;
 
-	start = 0;
-	end = ft_strlen(s1);
 	if (s1 == NULL)
 		return (NULL);
 	if (set == NULL)
 		return ((char *)s1);
+	start = 0;
+	end = ft_strlen(s1);
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	while (end > start && ft_strchr(set, s1[end - 1]))
